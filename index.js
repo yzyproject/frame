da7d0c12c88fc162cfb34fdff0d0763154559b40
yumeng <2250246493@qq.com>
2019-07-09 00:08:14 +0800

    token

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const app = new Koa()
 const bodyParser = require('koa-bodyparser')
 const Router = require('koa-router');
 const router = require("./router");
+const userAgent = require('koa-useragent');
 app.use(bodyParser({
     enableTypes:['json', 'form', 'text']
 }))
+app.use(userAgent);
 router(app)
 app.listen(3005, () => {
     console.log('demo2 is run 3005 ')
--- src/api/login.js
+++ src/api/login.js
@@ -1,16 +1,24 @@
 const Server = require("../model/server")
 const bodyParser = require('koa-bodyparser')
+const util = require("util");
+
 let server = new Server();
 class TestC {
     constructor () {}
-    async cc ( ctx ) {
+    async login ( ctx ) {
         let {options,filter,orderBy,startPops,limit} = JSON.parse(ctx.request.body);
         let obj = ctx.request.body;
+        let agnet =util.inspect(ctx.userAgent)
         let result = await server.find("admin",options,filter,orderBy,startPops,limit);
-        let res = {
-            status:"success",
-            array:result
+        let res = {};
+        if(result[0]){
+           res = {
+                status:"success",
+                array:result,
+                status:200
+            }
         }
+        
         ctx.response.body = res ;
     }
 }
