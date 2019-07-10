868a1c79a6d984c33ff7f0af930b6861f221078f
yumeng <2250246493@qq.com>
2019-07-10 23:24:25 +0800

    token 生成

--- src/api/login.js
+++ src/api/login.js
@@ -1,7 +1,7 @@
 const Server = require("../model/server")
 const bodyParser = require('koa-bodyparser')
 const util = require("util");
-
+const jwt = require('jsonwebtoken');
 let server = new Server();
 class TestC {
     constructor () {}
@@ -11,14 +11,26 @@ class TestC {
         let agnet =util.inspect(ctx.userAgent)
         let result = await server.find("admin",options,filter,orderBy,startPops,limit);
         let res = {};
+        let token ="";
         if(result[0]){
-           res = {
+            token = jwt.sign({
+                name:result[0].name
+            },'yzy012' //随便一点内容，撒盐：加密的时候混淆
+             ,{
+              expiresIn: 60 //60秒到期时间
+            });
+            res = {
                 status:"success",
                 array:result,
+                token:token,
                 code:200
             }
         }
-        
+        jwt.verify(token, 'yzy012', function (err, decoded) {
+            if (!err){
+               console.log(decoded.name); //会输出123，如果过了60秒，则有错误。
+            }
+        })
         ctx.response.body = res ;
     }
 }
